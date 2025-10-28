54cd927daab411ceafdb4f0fb8c0359c79856376
Jinyong <135928466+MDR-Evan@users.noreply.github.com>
2025-10-28 15:13:25 +0900

    ㅕㅔㅇㅁㅅㄷ

--- js/motionWall.js
+++ js/motionWall.js
@@ -2,7 +2,7 @@ const motionWall = document.getElementById("motionWall");
 
 function moveToMotionWall(event) {
   event.preventDefault();
-  window.location.href = "/motionWall/motionWall.html";
+  window.location.href = "./motionWall/index.html";
 }
 
 motionWall.addEventListener("click", moveToMotionWall);
