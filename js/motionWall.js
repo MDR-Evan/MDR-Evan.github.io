2253e076c4a04bc85d00d68f0cfaa4a4e5d434a8
Jinyong <135928466+MDR-Evan@users.noreply.github.com>
2026-07-03 12:58:18 +0900

    CHANGE firmware test page

--- js/motionWall.js
+++
@@ -1,8 +0,0 @@
-const motionWall = document.getElementById("motionWall");
-
-function moveToMotionWall(event) {
-  event.preventDefault();
-  window.location.href = "./motionWall/index.html";
-}
-
-motionWall.addEventListener("click", moveToMotionWall);
